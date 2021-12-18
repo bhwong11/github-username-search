6d648159e926e91682d07f60e41fc00942b9bc5a
bhwong11 <user@email.com>
2021-12-17 21:50:11 -0500

    search function working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {useState,useEffect} from 'react';
 
-function App() {
+
+
+interface UserData{
+  name:string;
+  email:string;
+  bio:string;
+  avatar_url:string;
+}
+
+type Service={
+  status:'initial'|'loading'|'loaded'|'error',
+  payload:UserData;
+}
+
+
+const App:React.FC=()=> {
+  const [searchTerm, setSearchTerm]=useState('');
+  const [userData,setUserData] = useState<Service>({
+    status:'initial',
+    payload:{
+    name:'',
+    email:'',
+    bio:'',
+    avatar_url:'',
+  }});
+  const fetchUserData= (username:String):void=>{
+    fetch(`https://api.github.com/users/${username}`)
+    .then((data:any)=>data.json())
+    .then((json:any)=>{
+      setUserData({status:'loaded',payload:json,})
+    })
+    .catch((err)=>
+      console.log(err)
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World!
+      <input 
+      value = {searchTerm} 
+      onChange={(e):any=>setSearchTerm(e.target.value)}/>
+      <button onClick={()=>fetchUserData(searchTerm)}>Search</button>
+      <button onClick={(e):any=>setUserData({status:'initial',payload:{
+        name:'',
+        email:'',
+        bio:'',
+        avatar_url:'',
+      }})}
+      >clear Search</button>
+      {userData.payload?<>
+        {userData.payload.name}
+        {userData.payload.bio}
+        {userData.payload.email}
+        {searchTerm}
+        <img src={userData.payload.avatar_url} alt={searchTerm}/>
+      </>:<></>}
+
     </div>
   );
 }
